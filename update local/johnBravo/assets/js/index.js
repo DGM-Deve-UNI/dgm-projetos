6ac5a6f05e402a76e6d6fee367f910d693a98d88
Daniel-DGPlay <bslab303c@uniacad.edu.br>
2024-09-23 20:45:14 -0300

    update index.html, home.html, styles.css, add home.css, update script.js

--- update local/johnBravo/assets/js/index.js
+++ update local/johnBravo/assets/js/index.js
@@ -1,26 +1,35 @@
-// ==== Carregar páginas no "main" do "index.html" ==== \\
-document.addEventListener('DOMContentLoaded', ()=>{
-    // Função para carregar conteúdo da página \\
+document.addEventListener('DOMContentLoaded', () => {
+    // Função para carregar conteúdo da página
     function loadPage(url) {
         fetch(url)
-        .then(response=> response.text())
-        .then(data=> {
+        .then(response => response.text())
+        .then(data => {
             document.getElementById('content').innerHTML = data;
+            loadCSS(url); // Carrega o CSS correspondente
         })
-        .catch(error=> console.error('Erro ao carregar a página:', error));
+        .catch(error => console.error('Erro ao carregar a página:', error));
     }
+
+    // Função para carregar o CSS correspondente
+    function loadCSS(url) {
+        const cssUrl = url.replace('.html', '.css'); // Supondo que o CSS tenha o mesmo nome do HTML
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = cssUrl;
+        document.head.appendChild(link);
+    }
+
     document.querySelectorAll('nav a').forEach(link => {
         const url = link.getAttribute('href'); // Obtém o URL da página
 
         if (!link.id) {
-            link.addEventListener('click',(e)=> {
+            link.addEventListener('click', (e) => {
                 e.preventDefault(); // Evita o comportamento padrão do link
-                loadPage(url); //Carrega a página
+                loadPage(url); // Carrega a página
             });
         }
     });
+
     // Carrega a página inicial ou o conteúdo padrão
     loadPage('assets/html/home.html');
 });
-
-// inputs login efeito
