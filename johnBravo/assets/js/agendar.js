fc4aa5e0e29799487b57fa003314c67107cb4e3c
matheusazz <113852948+matheusazz@users.noreply.github.com>
2024-11-21 13:37:14 -0300

    Add horarios

--- johnBravo/assets/js/agendar.js
+++ johnBravo/assets/js/agendar.js
@@ -137,7 +137,8 @@ document.addEventListener("DOMContentLoaded", function () {
         const timeSelect = document.getElementById("time");
         timeSelect.innerHTML = ""; // Limpa os horários
 
-        const times = ["09:00", "10:00", "11:00", "14:00", "15:00", "16:00"];
+        const times = ["08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30",  "13:00", "13:30", "14:00", "14:30", "15:00",
+             "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30"];
         times.forEach((time) => {
             const option = document.createElement("option");
             option.value = time;
